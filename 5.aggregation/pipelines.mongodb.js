01825a0ed5a8bd538e484be991191e709283d7b4
ssmarika <smarikashrestha02@gmail.com>
2024-07-07 19:44:42 +0545

    Initial Commit

--- 5.aggregation/pipelines.mongodb.js
+++ 5.aggregation/pipelines.mongodb.js
@@ -96,9 +96,69 @@ use("kec-crud");
 
 // ? find top 5 best rated Crime movies
 
+// db.movies.aggregate([
+//   { $match: { genres: "Crime" } },
+//   { $sort: { "rating.average": -1 } },
+//   { $limit: 5 },
+//   { $project: { name: 1, genres: 1, rating: "$rating.average", _id: 0 } },
+// ]);
+
+// ! PRACTICE
+
+// ? find movies whose rating is neither greater than 7 nor genres is "Drama"
+// db.movies.aggregate([
+//   { $match: { $nor: [{ "rating.average": { $gt: 7 } }, { genres: "Drama" }] } },
+//   { $project: { name: 1, genres: 1, _id: 0, rating: "$rating.average" } },
+// ]);
+
+// ?find movies whose rating is greater than 7 and less than 9
+// db.movies.aggregate([
+//   {
+//     $match: {
+//       $and: [
+//         { "rating.average": { $gt: 7 } },
+//         { "rating.average": { $lt: 9 } },
+//       ],
+//     },
+//   },
+//   { $project: { name: 1, rating: "$rating.average", _id: 0 } },
+// ]);
+
+// ?OR
+
+// db.movies.aggregate([
+//   { $match: { "rating.average": { $gt: 7, $lt: 9 } } },
+//   { $project: { name: 1, rating: "$rating.average", _id: 0 } },
+// ]);
+
+//? find movies whose status is Ended and runtime is 60
+// db.movies.aggregate([
+//   { $match: { status: "Ended", runtime: 60 } }, // and as default
+//   { $project: { name: 1, status: 1, runtime: 1, _id: 0 } },
+// ]);
+
+// ?find movies whose weight is 75 and network country is Canada
+// db.movies.aggregate([
+//   { $match: { weight: 75, "network.country.name": "Canada" } },
+//   {
+//     $project: { name: 1, weight: 1, country: "$network.country.name", _id: 0 },
+//   },
+// ]);
+
+// ? find movies whose rating average is not 9
+// db.movies.aggregate([
+//   { $match: { "rating.average": { $ne: 9 } } },
+//   { $project: { name: 1, rating: "$rating.average", _id: 0 } },
+// ]);
+
+// ? find movies whose status is neither "Ended" not genres is "Action"
+// db.movies.aggregate([
+//   { $match: { $nor: [{ status: "Ended" }, { genres: "Action" }] } },
+//   { $project: { name: 1, status: 1, genres: 1, _id: 0 } },
+// ]);
+
+// ? find movies whose genres includes 'Drama' , "Comedy" and "Action"
 db.movies.aggregate([
-  { $match: { genres: "Crime" } },
-  { $sort: { "rating.average": -1 } },
-  { $limit: 5 },
-  { $project: { name: 1, genres: 1, rating: "$rating.average", _id: 0 } },
+  { $match: { genres: { $all: ["Drama", "Comedy", "Action"] } } },
+  { $project: { name: 1, genres: 1, _id: 0 } },
 ]);
